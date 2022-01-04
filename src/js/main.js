711aa1ceb1afad5a62dae16f9be1473970c4fcca
arina <awwwrina@gmail.com>
2022-01-04 13:52:15 +0300

    phone number  mask

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
+import checkTextInputs from './modules/checkTextInputs';
 import forms from './modules/forms';
+import mask from './modules/mask';
 import modals from './modules/modals';
 import sliders from './modules/sliders';
 
@@ -7,4 +9,7 @@ window.addEventListener('DOMContentLoaded', () => {
     sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
     sliders('.main-slider-item', 'vertical');
     forms();
+    mask('[name="phone"]');
+    checkTextInputs('[name="name"]');
+    checkTextInputs('[name="message"]');
 });
